c68ce57169110f8e70aca648077270e7afb3d9c0
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-22 23:30:00 -0400

    controlador suma

---
+++ practica/Apiexamen/Controllers/SumaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Apiexamen.Controllers
+{
+    public class SumaController : ApiController
+    {
+        // GET: api/productoes
+        [HttpGet]
+        [Route("suma")]
+        public double suma(double x,double y)
+        {
+            return x+y;
+        }
+    }
+}
